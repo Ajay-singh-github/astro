296cd04d826216a07902c13acc6d2fc89b7773d6
Shiv Pratap <sp09singhwaghel@gmail.com>
2024-12-14 19:39:42 +0530

    add : banner in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -67,16 +67,10 @@ const Header = () => {
           LOGO
         </div>
         <div className="flex gap-3 md:gap-8 items-center w-full justify-end text-xs md:text-base">
-          <div className="rounded-full p-1 md:p-2 flex items-center justify-center md:gap-2 bg-primary-200 w-[8rem] md:w-max">
-            <CiSearch className="md:text-3xl opacity-50" />
-            <input
-              type="text"
-              className="border-none outline-none md:w-full w-1/2"
-              placeholder="search"
-            />
-            <div className="rounded-full p-1 px-2 text-primary-200 md:px-4 md:text-md bg-secondary-100 text-center cursor-pointer">
-              search
-            </div>
+        <div
+            className="p-1 md:p-2 rounded bg-white px-2 md:px-8 border border-secondary-100 cursor-pointer text-xl font-bold"
+          >
+            Todays special offer
           </div>
           {/* <div className="underline cursor-pointer">Register</div> */}
           <div
